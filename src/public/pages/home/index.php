a1adfc746c52dcc46ba894797aff23e7a218d9f9
fenozo <960gd7@gmail.com>
2023-03-20 13:47:24 +0300

    connexion à PDO sql server

--- src/public/pages/home/index.php
+++ src/public/pages/home/index.php
@@ -1,3 +1,9 @@
 <?php 
 
-echo 'Home';
+try {
+    $db = new PDO("sqlsrv:Server=192.168.123.141;Database=Commerciale", "sa", "commerciale");
+    echo 'PHP est connecté sur le serveur de base de données ! ';
+} catch (\Throwable $th) {
+    throw $th;
+}
+
