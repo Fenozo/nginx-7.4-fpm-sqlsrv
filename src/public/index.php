c4ead2069a9f3082ee01bb3db5a0b99cacb9ca6a
fenozo <960gd7@gmail.com>
2023-03-21 10:01:27 +0300

    connexion to db

--- src/public/index.php
+++ src/public/index.php
@@ -2,3 +2,17 @@
 
 
 phpinfo();
+
+
+$serverName = "(local)\sqlexpress"; 
+
+/* Connect using Windows Authentication. */  
+try  
+{  
+    $conn = new PDO( "sqlsrv:server=$serverName ; Database=commerciale", "sa", "commerciale");  
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
+}  
+catch(Exception $e)  
+{   
+    die( print_r( $e->getMessage() ) );   
+}  
